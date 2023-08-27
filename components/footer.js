2f902c0dd1b0aebd8a875522475024f70b3d29f4
Rafatulislam60 <rafatulislam67@gmail.com>
2023-08-28 00:37:35 +0600

    add footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,38 @@
+import Link from "next/link";
+import { ImFacebook, ImInstagram, ImYoutube } from "react-icons/im";
+import Newsletter from "./_child/newsletter";
+
 export default function footer() {
+  const bg = {
+    backgroundImage: "url('/images/footer.png')",
+    backgroundRepeat: "no-repeat",
+    backgroundPosition: "bottom left",
+  };
+
   return (
-    <div>footer</div>
-  )
+    <footer className="bg-gray-50" style={bg}>
+      <Newsletter />
+      <div className="container mx-auto flex justify-center py-12">
+        <div className="py-5">
+          <div className="flex gap-6 justify-center">
+            <Link href={"/"}>
+              <ImFacebook color="#888888" />
+            </Link>
+            <Link href={"/"}>
+              <ImInstagram color="#888888" />
+            </Link>
+            <Link href={"/"}>
+              <ImYoutube color="#888888" />
+            </Link>
+          </div>
+
+          <p className="py-5 text-gray-400">
+            Copyright ©2022 All rights reserved | This website & blogs owned by
+            Edu-Blogs.
+          </p>
+          <p className="text-gray-400 text-center">Terms & Condition</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
